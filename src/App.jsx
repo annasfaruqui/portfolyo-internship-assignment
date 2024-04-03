04bd2734d516d83ca865169a8c2bcfca2cda3c68
annasfaruqui <annasfaruqui123@gmail.com>
2024-04-03 16:24:30 +0530

    setup tailwind for styling the project

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   useEffect(function () {
     async function Fname() {
       const res = await fetch(
-        "https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae"
+        "https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae",
       );
       const data = await res.json();
 
